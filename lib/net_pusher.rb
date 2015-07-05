07935d16d48703bd4fa221454ad6e08a0cd50b83
Christian Becker <christian@beckerchristian.com>
2015-07-05 18:25:57 +0200

    Added error Handling for net pusher

--- lib/net_pusher.rb
+++ lib/net_pusher.rb
@@ -6,15 +6,17 @@ require_relative 'sml/sml_message'
 class NetPusher
   # Take the readings from reader and push it to a webservice
   def push(readings)
-    time = Time.now.to_i
     uri = URI.parse('http://192.168.0.21/reading.json')
-
-    readings.each do |r|
-      req = Net::HTTP::Post.new uri.path
-      req.body = { 'id' => r.id, 'value' => r.value, 'time' => r.time }.to_json
-      Net::HTTP.start(uri.host, uri.port) do |http|
-        http.request req
+    begin
+      readings.each do |r|
+        req = Net::HTTP::Post.new uri.path
+        req.body = { 'id' => r.id, 'value' => r.value, 'time' => r.time }.to_json
+        Net::HTTP.start(uri.host, uri.port) do |http|
+          http.request req
+        end
       end
+    rescue StandardError
+      return
     end
   end
 end
