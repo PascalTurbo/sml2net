31d59310af5b3a3bc548765d658d3f846f441e10
Christian Becker <christian@beckerchristian.com>
2015-07-05 17:25:22 +0200

    split the process into two differnt part. Implemented the client part

---
+++ client.rb
@@ -0,0 +1,30 @@
+require_relative '/lib/sml/sml_reader'
+
+# Start PV-Reader
+def start_pv
+  pid_pv = fork do
+    reader_pv = sml_reader.new(device: '/dev/lesekopf0')
+    reader_pv.start
+  end
+
+  pid_pv.dettach
+  pid_pv
+end
+
+# Start EVS-Reader
+def start_evs
+  pid_evs = fork do
+    reader_evs = sml_reader.new(device: '/dev/lesekopf1')
+    reader_evs.start
+  end
+
+  pid_evs.dettach
+  pid_evs
+end
+
+
+# 1. Pro Device einen eigenen Prozess erstellen
+# 2. Jeder Prozess sendet sein Ergebnis per http an einen "Webservice"
+# 3. Dieser Webservice sorgt für die Weiterverarbeitung
+
+
---
+++ lib/sml/sml_reader.rb
@@ -0,0 +1,29 @@
+require 'rubyserial'
+require 'sml_message'
+
+def intialize(params)
+  @device = params[:device]
+  @serialport = Serial.new @device, 9600
+  @running = false
+end
+
+def start
+  @running = true
+  sml = SmlMessage.new
+  while @running
+    byte = serialport.getbyte
+    if sml.finished?
+      # sml.readings.each do |r|
+      #   pusher.push(r)
+      # end
+      puts sml.to_s
+      sml = SmlMessage.new
+    else
+      sml << byte unless byte.nil?
+    end
+  end
+end
+
+def stop
+  @running = false
+end
