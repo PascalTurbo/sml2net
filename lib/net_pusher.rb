c09bf7cdcc9e2f06d7e8e2acff9628065a1ae4bf
root <root@raspberrypi.(none)>
2015-07-05 19:15:42 +0200

    Debugging net pusher

--- lib/net_pusher.rb
+++ lib/net_pusher.rb
@@ -6,7 +6,7 @@ require_relative 'sml/sml_message'
 class NetPusher
   # Take the readings from reader and push it to a webservice
   def push(readings)
-    uri = URI.parse('http://192.168.0.21:4567/reading')
+    uri = URI.parse('http://192.168.0.21:4567/reading.json')
 #    begin
       readings.each do |r|
         req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
